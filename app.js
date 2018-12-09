f907adc341476cac0e75e33af6389a7ca3be07a8
Nyapal Gatkuoth <nyapalgatkuoth@Nyapals-MacBook-Pro.local>
2018-12-08 20:07:50 -0800

    css files finally working

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const path = require('path');
+
+app.use(express.static(__dirname + '/public'));
+
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.engine('.html', require('ejs').__express);
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'html');
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html')
+    res.sendFile(__dirname + '/views/index.html')
 });
 
 app.get('/online', function(req, res) {
-    res.sendFile(__dirname + '/online.html')
+    res.sendFile(__dirname + '/views/online.html')
 });
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
       io.emit('chat message', msg)
-    console.log('message: ' + msg)
-
-    //io.emit('chat message', msg);
   });
 });
 
